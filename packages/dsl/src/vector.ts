79c39070996ba58b399d1321c92a5be330a2d621
feikerwu <feikerwu@gmail.com>
2022-11-03 18:45:06 +0800

    chore: vector 实现

--- packages/dsl/src/vector.ts
+++ packages/dsl/src/vector.ts
@@ -14,4 +14,22 @@ export class Vector {
   distance(): number {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
+
+  rotate(rad: number) {
+    const c = Math.cos(rad),
+      s = Math.sin(rad);
+
+    const { x, y } = this;
+    this.x = x * c + y * -s;
+    this.y = x * s + y * c;
+  }
+
+  copy() {
+    return new Vector(this.x, this.y);
+  }
+
+  scale(scale: number) {
+    this.x = this.x * scale;
+    this.y = this.y * scale;
+  }
 }
